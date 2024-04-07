91528d386146967c3739a470cb8d1b014c94aff5
bobizan <166299723+bobizan@users.noreply.github.com>
2024-04-07 14:50:25 +0000

    Update join.js

--- plugins/join.js
+++ plugins/join.js
@@ -1,11 +1,11 @@
-let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
+let linkRegex = /https://chat.whatsapp.com/Kifx24awOB4IpF2UQzWEjh \/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
 
 let handler = async (m, { conn, text, isOwner }) => {
     let [_, code, expired] = text.match(linkRegex) || []
     if (!code) throw 'الرابط غير صالح'
     let res = await conn.groupAcceptInvite(code)
     expired = Math.floor(Math.min(999, Math.max(1, isOwner ? isNumber(expired) ? parseInt(expired) : 0 : 3)))
-    m.reply(`تم الانضمام للمجموعة بنجاح ${res}${expired ? ` خلال ${expired} يوم` : ''}\n\nتابع صاحب البوت في حسابه\ninstagram.com/noureddine_ouafy`)
+    m.reply(`تم الانضمام للمجموعة بنجاح ${res}${expired ? ` خلال ${expired} يوم` : ''}\n\n*تابع صاحب البوت في حسابه* *https://instagram.com/hlimi_imran*`)
     let chats = global.db.data.chats[res]
     if (!chats) chats = global.db.data.chats[res] = {}
     if (expired) chats.expired = +new Date() + expired * 1000 * 60 * 60 * 24
