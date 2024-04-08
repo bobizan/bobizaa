bcb68d96bc6e0c6f51f540aeaef33579647973f0
bobizan <166299723+bobizan@users.noreply.github.com>
2024-04-08 02:21:02 +0000

    Update wm.js

--- plugins/wm.js
+++ plugins/wm.js
@@ -1,12 +1,12 @@
 import { addExif } from '../lib/sticker.js'
 let handler = async (m, { conn, text }) => {
-if (!m.quoted) throw '*الرجاء الاشارة الى الملصق الذي تريد أن تغير حقوقه مثال* : \n *.wm Imran_،hlimi*'
+if (!m.quoted) throw '*الرجاء الاشارة الى الملصق الذي تريد أن تغير حقوقه مثال* : \n *.wm Imran_hlimi*'
 let stiker = false
 try {
 let [packname, ...author] = text.split('|')
 author = (author || []).join('|')
 let mime = m.quoted.mimetype || ''
-if (!/webp/.test(mime)) throw '*الرجاء الاشارة الى الملصق الذي تريد أن تغير حقوقه مثال* : \n *.wm noureddine ouafy*'
+if (!/webp/.test(mime)) throw '*الرجاء الاشارة الى الملصق الذي تريد أن تغير حقوقه مثال* : \n *.wm Imran_hlimi*'
 let img = await m.quoted.download()
 if (!img) throw '*الرجاء  الاشارة الى الملصق الذي تريد أن تغير حقوقه مثال* : \n *.wm Imran_hlimi*'
 stiker = await addExif(img, packname || global.packname, author || global.author)
